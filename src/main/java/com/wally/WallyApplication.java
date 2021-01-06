08e7032494b1271e3796acd710b1dbdc415d7da8
Hiteshwar21 <hiteshwar.mehla@gmail.com>
2021-01-06 17:25:06 +0530

    First GIT Push

---
+++ src/main/java/com/wally/WallyApplication.java
@@ -0,0 +1,13 @@
+package com.wally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallyApplication.class, args);
+	}
+
+}
